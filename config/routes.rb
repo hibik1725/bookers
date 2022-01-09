42d82746b0953991145882ae1fbb523451d9e737
Keichan15 <keisuke1515gaming@outlook.jp>
2022-01-09 05:03:39 +0000

    [Add] controllerと対応Viewの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
+  get 'homes/about' => 'homes#about', as: 'about'
+
+  resources :users, only: [:index, :show, :edit]
+  resources :books, only: [:new, :index, :show, :edit]
+
 end
