b54921ece893d86c0461f1590e3a75b133ebb45a
Keichan15 <keisuke1515gaming@outlook.jp>
2022-07-29 17:46:34 +0000

    コメント機能のバリデーション実装

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,15 +1,22 @@
 class BookCommentsController < ApplicationController
 
   def create
-    book = Book.find(params[:book_id])
-    comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
-    comment.save
-    redirect_back(fallback_location: root_path)
+    @book = Book.find(params[:book_id])
+    @comment = current_user.book_comments.new(book_comment_params)
+    @comment.book_id = @book.id
+    if @comment.save
+      redirect_back(fallback_location: root_path)
+    else
+      @newbook = Book.new
+      @book_comment = BookComment.new
+      @user = @book.user
+      render 'books/show'
+    end
   end
 
   def destroy
-    BookComment.find(params[:id]).destroy
+    book = BookComment.find(params[:id])
+    book.destroy
     redirect_back(fallback_location: root_path)
   end
 
